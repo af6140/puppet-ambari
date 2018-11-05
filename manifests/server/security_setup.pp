d47bf31675954e9603632a47f4f3ceefad4d00be
Wang, Dawei <dwang@entertainment.com>
2018-11-05 16:35:45 -0500

    store path

--- manifests/server/security_setup.pp
+++ manifests/server/security_setup.pp
@@ -1,16 +1,18 @@
 # setup ssl cert
 class ambari::server::security_setup (
-  $ssl_cert_path,
-  $ssl_key_path,
-  $ssl_key_pass,
+  String[1] $ssl_cert_path,
+  String[1] $ssl_key_path,
+  Optional[String[1]] $ssl_key_pass,
 ) {
 
   $cmd = "ambari-server setup-security --security-option=setup-https --api-ssl=true --api-ssl-port=8443 --import-cert-path=${ssl_cert_path} --import-key-path=${ssl_key_path} --pem-password=${ssl_key_path}"
-
-  exec { 'run ambari-server security setup':
+  exec { 'ambari_server_security_setup':
     command => "${cmd} && touch /etc/ambari-server/conf/.security_setup",
     path    => '/bin:/sbin:/usr/bin:/usr/sbin',
     creates => '/etc/ambari-server/conf/.security_setup'
   }
+
+  #notify ambari server
+  Exec['ambari_server_security_setup'] ~> Service['ambari-server']
 
 }
---
+++ manifests/server/truststore.pp
@@ -0,0 +1,30 @@
+# setup ssl cert
+class ambari::server::truststore (
+  String[1] $trust_store_path='/var/lib/ambari-server/keys/keystore.p12',
+  String[1] $trust_store_pass='Changeme',
+  Array[String[1]] $cert_paths=[],
+) {
+  if size($cert_paths) >0 {
+    $cmd = "ambari-server setup-security --security-option=setup-truststore --truststore-path=${trust_store_path} --truststore-type=pkcs12 --truststore-password=${trust_store_pass}  --truststore-reconfigure"
+    exec { 'ambari_server_truststore_setup':
+      command => "${cmd} && touch /etc/ambari-server/conf/.truststore_setup",
+      path    => '/bin:/sbin:/usr/bin:/usr/sbin',
+      creates => '/etc/ambari-server/conf/.truststore_setup',
+    }
+
+    $cert_paths.each |$cert | {
+      $cert_path_spec=split($cert, '/')
+      $cert_file=$cert_path_spec[-1]
+      $cert_name=regsubst($cert_file, '\.crt$', '')
+      $import_cmd="ambari-server setup-security --security-option=import-certificate --truststore-path=${trust_store_path}  --truststore-type=pkcs12 --truststore-password=${trust_store_path} --import-cert-path=${cert} --import-cert-alias=${cert_name} --truststore-reconfigure"
+      exec {"setup_cert_${cert_name}":
+        command => "${cmd} && touch /etc/ambari-server/conf/.truststore_setup",
+        path    => '/bin:/sbin:/usr/bin:/usr/sbin',
+        creates => '/etc/ambari-server/conf/.truststore_setup',
+        require => Exec['ambari_server_truststore_setup'],
+        notify => Service['ambari-server'],
+      }
+    }
+  }
+
+}
