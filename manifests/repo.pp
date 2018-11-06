6af5c295b55eab6e046bbade1d1452e99cca3597
Wang, Dawei <dwang@entertainment.com>
2018-11-06 10:26:53 -0500

    no wget

--- manifests/repo.pp
+++ manifests/repo.pp
@@ -3,10 +3,9 @@ class ambari::repo {
   include ::ambari
 
   if $::osfamily == 'RedHat' {
-    wget::fetch { 'ambari_yum_repo':
+    file { "/etc/yum.repos.d/ambari_${::ambari::version}.repo" :
+      ensure => 'present',
       source      => "http://public-repo-1.hortonworks.com/ambari/centos${::operatingsystemmajrelease}/${::ambari::repo_version}/updates/${::ambari::version}/ambari.repo",
-      destination => "/etc/yum.repos.d/ambari_${::ambari::version}.repo",
-      execuser    => 'root',
       mode        => '0644'
     }
   }
