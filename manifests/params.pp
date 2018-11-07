40c5a771565d3ccfa66ae9c939c8c74c0fdee768
Wang, Dawei <dwang@entertainment.com>
2018-11-07 13:24:19 -0500

    default ambari to 2.7.1.0

--- manifests/params.pp
+++ manifests/params.pp
@@ -1,5 +1,5 @@
 class ambari::params {
-  $version = '2.1.0'
+  $version = '2.7.1.0'
   $jdbc_driver_url = 'http://dev.mysql.com/get/Downloads/Connector-J/mysql-connector-java-5.1.36.tar.gz'
   $service_name = 'ambari-server'
   $service_ensure = 'running'
