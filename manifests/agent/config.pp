ced16420a429f01d242b869895e59c3483f523a3
Wang, Dawei <dwang@entertainment.com>
2018-11-07 17:21:42 -0500

    configs

--- manifests/agent/config.pp
+++ manifests/agent/config.pp
@@ -1,21 +1,22 @@
-class ambari::agent::config() {
+class ambari::agent::config (
+  $custom_host_name = $::ambari::agent::agent_host_name,
+  $public_host_name = $::ambari::agent::agent_public_host_name,
+) {
 
   $ambari_server = $::ambari::agent::ambari_server
   $ambari_server_port = $::ambari::agent::ambari_server_port
   $ambari_server_secure_port = $::ambari::agent::ambari_server_secure_port
 
-  $host_name = ::ambari::agent::agent_host_name
-  $public_host_name = ::ambari::agent::agent_public_host_name
-  if  $host_name {
+  if  $custom_host_name {
     file {'/var/lib/ambari-agent/hostname.sh':
       ensure => 'present',
-      content => "#!/bin/sh\necho ${hostname}",
+      content => "#!/bin/sh\necho ${custom_host_name}",
       mode => '0755',
       require => Package[$::ambari::agent::package_name],
       before => File['/etc/ambari-agent/conf/ambari-agent.ini']
     }
   }
-  if public_host_name {
+  if $public_host_name {
     file {'/var/lib/ambari-agent/public_hostname.sh':
       ensure => 'present',
       content => "#!/bin/sh\necho ${public_host_name}",
